35cc1fc99fed85214364a6e79c7c7e00907af582
[Diego <d.a.malvasio@gmail.com>
2020-11-26 23:24:28 -0300

    Initial login and homescreen

---
+++ components/CustomBttn.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import * as RootNavigation from '../navigation/RootNavigation';
+import { Text, TouchableOpacity } from 'react-native';
+import Color from '../constants/Colors'
+
+export default function CustomBttn({name, dest}) {
+    return (
+        <TouchableOpacity style={{ marginTop:20, width:200, height:50, backgroundColor:Color.primary, padding:10, alignItems:'center', borderRadius:5}} 
+          onPress={()=> RootNavigation.navigate(dest)}>
+          <Text style={{color:Color.white, fontSize:20}}>{name}</Text>
+        </TouchableOpacity>
+    );
+}
---
+++ constants/Colors.js
@@ -0,0 +1,8 @@
+export default {
+    primary: '#01cdfe',
+    accent: '#ff71ce',
+    vaporC1: '#05ffa1',
+    vaporC2: '#b967ff',
+    vaporC3: '#fffb96',
+    white: '#fff',
+};
